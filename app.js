16f547c92bd6ff15d26f5d2f5874015e0142c4ee
Tim <tim.weinert@gmail.com>
2020-01-23 12:40:13 +1100

    app comments updated

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-//Variables for hand selector in document
+//Variables to link to each different hand in the HTML document (DOM)
 const secondsHand = document.querySelector('.second-hand');
 const minutesHand = document.querySelector('.min-hand');
 const hoursHand = document.querySelector('.hour-hand');
 
 
-
 //Set function to get the time
-function setDate(){
-    //Variable now = the new Date constructor (find out more)
+function setDate() {
+    //Variable now = the new Date constructor (method )
     const now = new Date();
+
     //Establish variables for seconds, minutes and hours
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
-    //Establish variable for hand position according to time (360deg)
+
+    //Establish variable for hand position on clock face according to time (using 360deg)
     //+90 to offset the transform 90deg in stylesheet (to make the clock start from 0)
-    const secondsDegrees = ((seconds / 60) * 360) + 90; 
-    const minutesDegrees = ((minutes / 60) * 360) + 90; 
-    const hoursDegrees = ((hours / 12) * 360) + 90; 
-   
-    //Second hand is selected and rotated to the correct degrees established above
-    // ` (${ }) ` is a template literal - the `` allow an embedded expression (referral to the const secondsDegrees)
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+
+    //Element.change styling.which css property?
+    // ` ` = what we want to do  ${ } = a variable set out previously (above)
+    //Rotate the seconds hand to the degrees calculated in the variable
     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`
     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`
     hoursHand.style.transform = `rotate(${hoursDegrees}deg)`
